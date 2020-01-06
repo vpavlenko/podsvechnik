d5b5068798f262ededc79d2292927fe37cbb198d
Vitaly Pavlenko <cxielamiko@gmail.com>
2020-01-06 14:48:57 -0500

    Try to make fetch request, hitting CORS errors

---
+++ chrome_dev_script.js
@@ -0,0 +1,17 @@
+document.getElementsByTagName('p')
+
+var data = new FormData();
+data.append('text', 'This is a syntax highlighter for English. It uses natural language processing to determine the part of speech for each word and highlights accordingly. It works with tricky sentences too, e.g. The man we saw saw a saw.');
+
+const response = await fetch('https://english.edward.io/parse', {
+  method: 'POST', // *GET, POST, PUT, DELETE, etc.
+  mode: 'no-cors', // no-cors, *cors, same-origin
+  // credentials: 'same-origin', // include, *same-origin, omit
+  headers: {
+    // 'Content-Type': 'application/json'
+    // 'Content-Type': 'application/x-www-form-urlencoded',
+  },
+  // redirect: 'follow', // manual, *follow, error
+  // referrerPolicy: 'no-referrer', // no-referrer, *client
+  body: data
+});
