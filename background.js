a86d0ad96ec3dae76c037067fbbf4e23a65807b4
Vitaly Pavlenko <cxielamiko@gmail.com>
2020-01-08 11:41:07 -0500

    Request API from plugin on selected text

--- background.js
+++ background.js
@@ -8,6 +8,20 @@ chrome.runtime.onInstalled.addListener(function() {
 
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
     if (info.menuItemId === "menu1") { // here's where you'll need the ID
-        alert('yay');
+      var data = new FormData();
+      data.append('text', info.selectionText);
+
+      fetch('https://english.edward.io/parse', {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'no-cors', // no-cors, *cors, same-origin
+        // credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+          // 'Content-Type': 'application/json'
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        // redirect: 'follow', // manual, *follow, error
+        // referrerPolicy: 'no-referrer', // no-referrer, *client
+        body: data
+      }).then(response => response.text()).then(data => { alert(data); });
     }
 });
--- chrome_dev_script.js
+++ chrome_dev_script.js
@@ -1,9 +1,7 @@
-document.getElementsByTagName('p')
-
 var data = new FormData();
 data.append('text', 'This is a syntax highlighter for English. It uses natural language processing to determine the part of speech for each word and highlights accordingly. It works with tricky sentences too, e.g. The man we saw saw a saw.');
 
-const response = await fetch('https://english.edward.io/parse', {
+fetch('https://english.edward.io/parse', {
   method: 'POST', // *GET, POST, PUT, DELETE, etc.
   mode: 'no-cors', // no-cors, *cors, same-origin
   // credentials: 'same-origin', // include, *same-origin, omit
@@ -14,4 +12,4 @@ const response = await fetch('https://english.edward.io/parse', {
   // redirect: 'follow', // manual, *follow, error
   // referrerPolicy: 'no-referrer', // no-referrer, *client
   body: data
-});
+}).then(response => response.text()).then(data => { alert(data); });
